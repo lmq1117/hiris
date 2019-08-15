2c4c660fdb1ee29e219a18ca311f6bfce0d54757
limq <limq@cgnfs.com.cn>
2019-08-15 17:27:37 +0800

    Not 操作

--- main.go
+++ main.go
@@ -40,21 +40,39 @@ func main() {
 	//fmt.Println(users[1].CreatedAt)
 
 	//按主键获取
-	db.First(&user, 10)
+	//db.First(&user, 10)
 
 	//简单sql
-	db.Find(&user, "name = ?", "孔明")
-	db.Find(&users, "name <> ? AND age > ?", "黄忠", 55)
+	//db.Find(&user, "name = ?", "孔明")
+	//db.Find(&users, "name <> ? AND age > ?", "黄忠", 55)
 
 	//Struct
-	db.Find(&users, models.User{Age: 55})
-	db.Find(&users, map[string]interface{}{"age": 60})
+	//db.Find(&users, models.User{Age: 55})
+	//db.Find(&users, map[string]interface{}{"age": 60})
 
+	//================= Not 条件
+	// SELECT * FROM users WHERE name <> "jinzhu" LIMIT 1;
+	//db.Not("name","刘备").First(&user)
 
+	//SELECT * FROM user WHERE name NOT IN ("曹操", "曹丕");
+	//db.Not("name",[]string{"曹操","曹丕"}).Find(&users)
+
+	//SELECT * FROM users WHERE id NOT IN (1,2,3);
+	//db.Not([]int64{1,2,3}).First(&user)
+
+	//SELECT * FROM users limit 1
+	//db.Not([]int64{}).First(&user)
+
+	//Plain sql
+	//db.Not("name = ?","孔明").First(&user)
+
+	//struct
+	//db.Not(models.User{Name:"刘备"}).First(&user)
+
+	//===========带内联条件的查询
 
 	fmt.Println(user)
 	fmt.Println(users)
-
 
 	defer db.Close()
 }
