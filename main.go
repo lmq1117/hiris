ace242fc4da3647ab38be3cec5fed2dc8f98471d
limq <limq@cgnfs.com.cn>
2019-09-20 16:49:09 +0800

    Routing override context **

--- main.go
+++ main.go
@@ -2,44 +2,52 @@ package main
 
 import (
 	"github.com/kataras/iris"
-	"net/http"
-	"strings"
+	"github.com/kataras/iris/context"
+	"reflect"
 )
 
-func main() {
-	app := newApp()
-	app.Run(iris.Addr(":8080"))
+type MyContext struct {
+	iris.Context
 }
 
-func newApp() *iris.Application {
+var _ iris.Context = &MyContext{}
+
+func (ctx *MyContext) Do(handler context.Handlers) {
+	context.Do(ctx, handler)
+}
+
+func (ctx *MyContext) Next() {
+	context.Next(ctx)
+}
+
+func (ctx *MyContext) HTML(format string, args ...interface{}) (int, error) {
+	ctx.Application().Logger().Infof("Executing .HTML function from MyContext")
+	ctx.ContentType("text/html")
+	return ctx.Writef(format, args...)
+}
+
+func main() {
 	app := iris.New()
-	app.OnErrorCode(iris.StatusNotFound, func(ctx iris.Context) {
-		ctx.HTML("<b>资源未找到</b>")
-	})
-
-	//todo ServerFile 干嘛的呀
-	app.Get("/abc", func(ctx iris.Context) {
-		ctx.ServeFile("./public/index.html", false)
-	})
-	app.Get("/profile/{username}", func(ctx iris.Context) {
-		ctx.Writef("Hello %s", ctx.Params().Get("username"))
-	})
-
-	app.HandleDir("/", "./public")
-
-	myOtherHandler := func(ctx iris.Context) {
-		ctx.Writef("在我们的自定义路由器包装器手动触发的处理程序内部")
-	}
-
-	app.WrapRouter(func(w http.ResponseWriter, r *http.Request, route http.HandlerFunc) {
-		path := r.URL.Path
-		if strings.HasPrefix(path, "/other") {
-			ctx := app.ContextPool.Acquire(w, r)
-			myOtherHandler(ctx)
-			app.ContextPool.Release(ctx)
-			return
+	app.ContextPool.Attach(func() iris.Context {
+		return &MyContext{
+			Context: context.NewContext(app),
 		}
-		route.ServeHTTP(w, r)
 	})
-	return app
+	app.RegisterView(iris.HTML("./views", ".html"))
+	app.Handle("GET", "/", recordWhichContextForExample, func(ctx iris.Context) {
+		ctx.HTML("<h1>Hello from my custom context 's HTML! </h1>")
+	})
+	app.Handle("GET", "/hi/{name:alphabetical}", recordWhichContextForExample, func(ctx iris.Context) {
+		name := ctx.Params().GetString("name")
+		ctx.ViewData("name", name)
+		ctx.Gzip(true)
+		ctx.View("hi.html")
+	})
+	app.Run(iris.Addr(":8080"))
+
+}
+
+func recordWhichContextForExample(ctx iris.Context) {
+	ctx.Application().Logger().Infof("(%s) Handler is executing from:'%s'", ctx.Path(), reflect.TypeOf(ctx).Elem().Name())
+	ctx.Next()
 }
